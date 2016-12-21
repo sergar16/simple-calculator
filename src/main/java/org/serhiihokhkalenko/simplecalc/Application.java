b07f628b20ad1ece7f4dfda23f01f436581d4878
Serhii Hokhkalenko <serhii.hokhkalenko@gmail.com>
2016-12-21 23:55:50 +0100

    moving Application to right folder

--- src/main/java/org/serhiihokhkalenko/simplecalc/Application.java
+++ src/main/java/org/serhiihokhkalenko/simplecalc/Application.java
@@ -1,4 +1,4 @@
-package org.serhiihokhkalenko;
+package org.serhiihokhkalenko.simplecalc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
