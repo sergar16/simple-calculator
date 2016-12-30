ca2b26ce32d56bfe3ca4fa7074ab4f2e58aa6e18
Serhii Hokhkalenko <serhii.hokhkalenko@gmail.com>
2016-12-31 01:16:06 +0200

    structure of Expresion

--- src/main/java/org/serhiihokhkalenko/simplecalc/dsl/model/expresion/Expresion.java
+++ src/main/java/org/serhiihokhkalenko/simplecalc/dsl/model/expresion/Expresion.java
@@ -1,8 +1,12 @@
 package org.serhiihokhkalenko.simplecalc.dsl.model.expresion;
 
+import org.serhiihokhkalenko.simplecalc.dsl.model.ArithmeticValue;
 import org.serhiihokhkalenko.simplecalc.dsl.model.Value;
 import org.serhiihokhkalenko.simplecalc.dsl.model.operations.BinaryOperation;
+import org.serhiihokhkalenko.simplecalc.dsl.model.operations.BinaryOperations;
+import org.serhiihokhkalenko.simplecalc.dsl.model.operations.arithmetic.ArithmeticOperation;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -12,8 +16,39 @@ import java.util.List;
  * Date: 12/20/16
  * Time: 12:45 AM
  */
-public class Expresion {
-    Value value;
-   // List<va>
-    //todo @serhii:  inner Expresions expresion (expresion+expresion)
+public class Expresion implements ArithmeticValue{
+
+    //todo @serhii: make class imutable
+    //todo @serhii: use Imutable collection
+
+    private Expresion innerExpresion;
+    private ArithmeticOperation arithmeticOperation;
+
+    public static Expresion getExpresion(Expresion expresion, BinaryOperation binaryOperation){
+
+    }
+
+    public static Expresion getExpresion(ArithmeticValue , BinaryOperation){
+
+    }
+
+    @Override
+    //todo serhii: Arithmetic Value instead of Value in method signature
+    public ArithmeticValue performOperation(BinaryOperations operations, Value value) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getResult() {
+        return null;
+    }
+
+
+    public Expresion getInnerExpresion() {
+        return innerExpresion;
+    }
+
+    public ArithmeticOperation getArithmeticOperation() {
+        return arithmeticOperation;
+    }
 }
